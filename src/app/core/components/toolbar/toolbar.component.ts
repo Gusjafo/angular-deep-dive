5dca6658f65c1e8ef5ce80c29fd32c902f012d85
Gustavo.Fonollosa <gustavojafo@gmail.com>
2025-04-26 16:28:29 +0200

    fixing

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -3,7 +3,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { ThemeService } from '@core/services/theme.service';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-toolbar',
@@ -14,6 +14,7 @@ import { AsyncPipe, NgIf } from '@angular/common';
         RouterModule,
         AsyncPipe,
         NgIf,
+        CommonModule
     ],
     templateUrl: './toolbar.component.html',
     styleUrl: './toolbar.component.scss'
